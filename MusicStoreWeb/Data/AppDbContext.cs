23ae9e23a7f55dbf5b43d761a17e4256836f2aa4
Shay Johnson <shjohnson413@outlook.com>
2022-01-13 10:33:24 -0800

    Added Catgory Model. Installed Entity and SqlServer Entity, Added DbContext Class, Set up Services to Connect to DbContext and SqlServer. Created DbSet Categories.

---
+++ MusicStoreWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStoreWeb.Models;
+
+namespace MusicStoreWeb.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        DbSet<Category>? Categories { get; set; } 
+    }
+}
