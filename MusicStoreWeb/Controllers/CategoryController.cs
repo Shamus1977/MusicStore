f9a3a844857f24a942a6be60daa81552bd573d11
Shay Johnson <shjohnson413@outlook.com>
2022-01-16 12:34:59 -0800

    Added Edit and Delete functionality to Category. Added Client side validation to Create Category Form. Added Display Name to Category Model.  Added Temp Data to CRUD methods for Catagory.

--- MusicStoreWeb/Controllers/CategoryController.cs
+++ MusicStoreWeb/Controllers/CategoryController.cs
@@ -18,6 +18,7 @@ namespace MusicStoreWeb.Controllers
             return View(categoryList);
         }
 
+        /// *************************    Create Section       ********************
         //Get
         public IActionResult Create()
         {
@@ -38,9 +39,74 @@ namespace MusicStoreWeb.Controllers
             {
                 _dbContext?.Categories?.Add(category);
                 _dbContext?.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             return View(category);
+        }
+
+        /// *************************    Edit Section       ********************
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var foundCategory = _dbContext?.Categories?.Find(id);
+            if (foundCategory == null)
+            {
+                return NotFound();
+            }
+            return View(foundCategory);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name",
+                    "The Display Order Cannot Exactly Match The Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext?.Categories?.Update(category);
+                _dbContext?.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // ****************************   Delete section   ******************
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var foundCategory = _dbContext?.Categories?.Find(id);
+            if (foundCategory == null)
+            {
+                return NotFound();
+            }
+            return View(foundCategory);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            _dbContext?.Categories?.Remove(category);
+            _dbContext?.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     }
 }
