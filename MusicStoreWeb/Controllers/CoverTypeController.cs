496626dbf18d3be2be31f737f237f8c6f04d203c
Shay Johnson <shjohnson413@outlook.com>
2022-02-08 16:00:49 -0600

    Added CoverType Controller. Added Index() method to CoverType Conteoller. Added Create() methods to CoverTypeController. Added Index view for CoverType Controller. Added Create view for CoverTypeController.

--- MusicStoreWeb/Controllers/CoverTypeController.cs
+++ MusicStoreWeb/Controllers/CoverTypeController.cs
@@ -1,9 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicStoreWeb.Models;
+using MusicStoreWeb.Repository.IRepository;
 
 namespace MusicStoreWeb.Controllers
 {
     public class CoverTypeController : Controller
     {
+        private readonly IUnitOfWork? _unitOfWork;
+        private ICoverTypeRepository? _repo;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _repo = _unitOfWork.CoverTypeRepository;
+        }
+
+        // //////////////// Get All Section ////////////////////////////////////////////////
+        
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> coverTypeList = _repo.GetAll();
+            return View(coverTypeList);
+        }
+
+        //////////////////////////////////////////  Create Section //////////////////////////////
+        
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType coverType)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo?.Add(coverType);
+                _unitOfWork?.Save();
+                TempData["success"] = "Cover Type Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(coverType);
+        }
 
     }
 }
